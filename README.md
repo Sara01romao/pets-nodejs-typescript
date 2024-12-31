229027cb059518e4bd4514c63e61f41c4cc7ec3f
Sara Romao <sara-romao@live.com>
2024-12-30 22:56:24 -0300

    feat: rotas informações dinamica banner e titulo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pets-nodejs-typescript
+
+## Pré-requisitos globais:
+
+npm i -g nodemon typescript ts-node
+
+## Instalação
+npm install
+
+## Rodar Projeto
+npm run start-dev
--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,45 @@
 import { Request, Response } from "express";
 
+import {createMenuObject} from '../helpers/createMenuObjcts'
+
 export const home = (req: Request, res: Response) =>{
-    res.send('home');
+    // res.send('home');
+    res.render('pages/page', {
+        menu:createMenuObject('all'),
+        banner:{
+            title: "Todos os Animais",
+            background: 'allanimals.jpg'
+
+        }
+    });
 }
 export const dogs = (req: Request, res: Response) =>{
+    res.render('pages/page', {
+        menu:createMenuObject('dog'),
+        banner:{
+            title: "Cachorros",
+            background: 'banner_dog.jpg'
 
+        }
+    });
 }
 export const cats = (req: Request, res: Response) =>{
+    res.render('pages/page', {
+        menu:createMenuObject('cat'),
+        banner:{
+            title: "Gatos",
+            background: 'banner_cat.jpg'
 
+        }
+    });
 }
 export const fishes = (req: Request, res: Response) =>{
+    res.render('pages/page', {
+        menu:createMenuObject('fish'),
+        banner:{
+            title: "Peixes",
+            background: 'banner_fish.jpg'
 
+        }
+    });
 }
